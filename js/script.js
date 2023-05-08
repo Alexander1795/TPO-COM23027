8e8a25991935dba1f6ee686b0af07be7c1f04f88
Alejandro Reinoso <aler_925@hotmail.com>
2023-05-08 19:59:39 -0300

    API valor dolar en aside
    Agregado de API cotización del dólar en índex y en script

--- js/script.js
+++ js/script.js
@@ -13,3 +13,20 @@ cerrar.addEventListener("click", () => {
     navegador.classList.toggle("visible")
     abrir.classList.toggle("invisible")
 })
+
+fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
+			.then(response => response.json()) // Convertir la respuesta a JSON
+			.then(data => {
+				// Procesar los datos
+				const valores = data.filter(item => item.casa.nombre.includes('Dolar'));
+
+				// Mostrar los valores en la tabla
+				const table = document.getElementById('valores-table');
+				valores.forEach(item => {
+					const row = table.insertRow();
+					row.insertCell(0).innerHTML = item.casa.nombre;
+					row.insertCell(1).innerHTML = item.casa.compra;
+					row.insertCell(2).innerHTML = item.casa.venta;
+				});
+			})
+			.catch(error => console.error(error));
